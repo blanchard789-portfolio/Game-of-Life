a0b69303329ed4d104d56572d4890d858545bb45
Hayden Blanchard <blanchardhayden45@gmail.com>
2026-02-01 17:00:39 -0800

    updated button controls and added display code

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod life;
 use life::*;
 
 use cortex_m_rt::entry;
-use embedded_hal::{delay::DelayNs, digital::InputPin};
+use embedded_hal::digital::InputPin;
 #[rustfmt::skip]
 use microbit::{
     board::{Board, Buttons},
@@ -18,7 +18,7 @@ use microbit::{
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
-use nanorand::{Rng, SeedableRng};
+use nanorand::{Pcg64, Rng, SeedableRng};
 
 /*
 enum State {
@@ -32,11 +32,14 @@ fn init() -> ! {
     rtt_init_print!();
     let mut board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
+    let mut rng = nanorand::Pcg64::new_seed(1);
+    let mut display = Display::new(board.display_pins);
 
     let mut button_a = board.buttons.button_a;
     let mut button_b = board.buttons.button_b;
 
-    let mut game_board = random_board();
+    let mut game_board: [[u8; 5]; 5] = [[0; 5]; 5];
+    random_board(&mut rng, &mut game_board);
     let mut b_frame_pause = 0;
 
     let mut end_game_check = false;
@@ -45,9 +48,10 @@ fn init() -> ! {
     loop {
         if button_a.is_low().unwrap() {
             rprintln!("a button");
-            game_board = random_board();
+            random_board(&mut rng, &mut game_board);
         } else if button_b.is_low().unwrap() && b_frame_pause == 0 {
             rprintln!("b button");
+            flip_pixels(&mut game_board);
             b_frame_pause = 5;
         }
 
@@ -63,31 +67,41 @@ fn init() -> ! {
         } else if done(&game_board) && no_pixel_pause == 0 {
             end_game_check = false;
             no_pixel_pause = 0;
-            game_board = random_board();
+            random_board(&mut rng, &mut game_board);
         } else {
             end_game_check = false;
             no_pixel_pause = 0;
         }
 
-        timer.delay_ms(100);
+        life(&mut game_board);
+        display.show(&mut timer, game_board, 100);
     }
 }
 
 // fn random_board -> [[u8; 5]; 5]
 // Generates a random 5x5 matrix of u8.
 // Used for creating new game boards.
-#[allow(clippy::needless_return)]
-fn random_board() -> [[u8; 5]; 5] {
-    let mut rng = nanorand::Pcg64::new_seed(1);
-    let mut rng_board = [[0; 5]; 5];
-
+fn random_board(rng: &mut Pcg64, rng_board: &mut [[u8; 5]; 5]) {
     #[allow(clippy::needless_range_loop)]
     for row in 0..5 {
         for col in 0..5 {
-            let b: u8 = rng.generate();
-            rng_board[row][col] = b;
+            let b: bool = rng.generate();
+            rng_board[row][col] = b as u8;
         }
     }
+}
 
-    return rng_board;
+// fn flip_pixels -> [[u8; 5]; 5]
+// Flips all pixels in 5x5 matrix.
+fn flip_pixels(board_in: &mut [[u8; 5]; 5]) {
+    #[allow(clippy::needless_range_loop)]
+    for row in 0..5 {
+        for col in 0..5 {
+            if board_in[row][col] == 1 {
+                board_in[row][col] = 0;
+            } else {
+                board_in[row][col] = 1;
+            }
+        }
+    }
 }
